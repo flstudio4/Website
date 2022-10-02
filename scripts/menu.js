5ae0c3f4affc0c90cf0d0d27569e697e9b755a6e
D.S <64180264+flstudio4@users.noreply.github.com>
2022-10-01 23:24:14 -0500

    Added mobile menu with animations, corrections, refactoring

---
+++ scripts/menu.js
@@ -0,0 +1,13 @@
+/*
+Mobile menu with animation
+ */
+
+"use strict";
+
+    function openNav() {
+        document.getElementById("mySidePanel").style.width = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidePanel").style.width = "0";
+    }
